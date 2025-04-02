d63287488fee57c6be53330f2353cc498fb2e839
rasha-2k <rasha.k.alsaleh@gmail.com>
2025-04-02 05:42:12 +0300

    Add loading screen and fix CSS conflicts

--- backend/test/responseTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../helpers/response.php';
-
-//test success with extra details
-sendSuccess("User registered successfully", ["user_id" => 123], 201);
-
-// Test error with extra details
-sendError("Validation failed", 422, ["fields" => ["email" => "Invalid email address"]]);
-
